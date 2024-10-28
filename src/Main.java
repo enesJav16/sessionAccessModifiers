4e8acb44bd0713d18c14fd6bccb45d3c17e62919
enesJav16 <maratuenes@gmail.com>
2024-10-28 22:28:16 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product[] products= {new Book(1L,"book","Book1","Book1's author"),
+                             new Book(2L,"book","Book2","Book2's author"),
+                             new Electronics(3L,"electronics","Phone","Samsung")};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        User[]users={new User(1L,"enes","marat uulu","enes@","enes1234",products)};
+        User user=new User();
+        user.setUsersArr(users);
+
+        user.start();
     }
 }
